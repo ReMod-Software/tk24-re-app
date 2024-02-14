c68674c548e8327a57597396affc9d63b9222bb8
LePichu <ishat.prsnl65844@gmail.com>
2024-02-14 19:21:08 +0530

    feat: setup tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss'],
+  css: [
+    "~/assets/css/main.css"
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {}
+    }
+  }
 })
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1 class="text-purple-500 text-2xl">
+    Beacon
+  </h1>
+</template>
